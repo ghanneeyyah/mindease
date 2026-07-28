0bae1254ef5dab4aee417c9b4cb73ad7a40ed528
ghanneeyyah <olaiwonganiyat18@gmail.com>
2026-07-28 14:25:12 +0100

    Revert "Prepare project for deployment."
    This reverts commit 77d6e3d9da7975137eda0a7edb3b4b815baf2094.

--- backend/emotion-service/main.py
+++ backend/emotion-service/main.py
@@ -260,5 +260,4 @@ async def analyze_debug(request: TextRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 5000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
